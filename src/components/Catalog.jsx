329e6b0773c3405dcf2e3a16abf6d3342bacf660
gwapesofwath <gwapesofwath@gmail.com>
2019-02-01 16:38:30 -0600

    initial changes

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -5,7 +5,25 @@ import Beer from './Beer';
 
 
 class Catalog extends Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      beerslist: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(results=>results.json())
+    .then(beers=> {
+      this.setState({
+        beerslist: beers
+      })
+    })
+  }
+
   render(){
+    console.log(this.state.beerslist)
     return(
       <div className="catalog">
         <div className="card shadow p-3 mb-5 bg-white rounded">
