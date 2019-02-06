db1be13d13a3a13232f2b7f09130d58dd49e7483
gwapesofwath <gwapesofwath@gmail.com>
2019-02-06 16:52:09 -0600

    stylistic changes

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -22,14 +22,16 @@ class Catalog extends Component {
   render() {
     console.log(this.state.beerslist);
     return (
-      <div className="bg-dark">
+      <div className="bg-light">
         <div className="banner" />
         <div >
           {this.state.beerslist.map(item => (
             <div key={item.id} className="catalog">
               <div className="item">
-                <h3>{item.name}</h3>
+                <h3>{item.name}</h3>       
+                <p>ABV: {item.abv}</p>
                 <p>{item.description}</p>
+                <p><h5>Food to Pair With:</h5>{item.food_pairing[0]}<li>{item.food_pairing[1]}</li><li>{item.food_pairing[2]}</li></p>
                 <img src={item.image_url} alt={item.tagline} />
               </div>
             </div>
